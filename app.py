60d9ceeefc231ba3f65e4b757574576b932256a7
zuriel <zuriellopezcisneros8@gmail.com>
2026-06-17 17:12:06 -0600

    NUEVA VERSION DE IA

--- app.py
+++ app.py
@@ -411,7 +411,7 @@ Zedith:
                 )
 
                 model = genai.GenerativeModel(
-                    model_name="gemini-1.5-flash",
+                    model_name="gemini-2.5-flash",
                     system_instruction=system_instruction_base
                 )
 
